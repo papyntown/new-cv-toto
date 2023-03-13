6e7a422b74ea4bb256b9d08d432c3b86275a74ac
papyntown <realistedem@gmail.com>
2023-03-13 14:17:16 +0100

    darkmode

--- components/DarkMode.js
+++ components/DarkMode.js
@@ -1,38 +1,46 @@
 import { useEffect, useState } from "react";
 
 function Darkmode() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(
+        typeof window !== "undefined" &&
+            localStorage.getItem("isDarkMode") === "true"
+    );
     useEffect(() => {
-        // je prend tous élement que je veux modifier et leur ajoute ma classe
         const elements = document.querySelectorAll(
             "body,a, .button, h1, p, h3, .random-circle, .project, .home , .navigation, .img-cv"
         );
+
         function toggleDarkMode() {
-            elements.forEach((element) => {
-                if (isDarkMode) {
-                    // Si le mode sombre est activé, j'ajoute la classe "dark-mode"
+            if (isDarkMode) {
+                elements.forEach((element) => {
                     element.classList.add("dark-mode");
-                    // Et je retire la classe "light-mode"
                     element.classList.remove("light-mode");
-                } else {
-                    // Sinon, j'ajoute la classe "light-mode"
+                });
+            } else {
+                elements.forEach((element) => {
                     element.classList.add("light-mode");
-                    // Et je retire la classe "dark-mode"
                     element.classList.remove("dark-mode");
-                }
-            });
+                });
+            }
         }
+
         toggleDarkMode();
     }, [isDarkMode]);
 
     function handleToggle() {
-        setIsDarkMode(!isDarkMode);
+        const newValue = !isDarkMode;
+        setIsDarkMode(newValue);
+        localStorage.setItem("isDarkMode", newValue);
     }
 
     return (
         <div className={`toggle ${isDarkMode ? "dark-mode" : ""}`}>
             <label>
-                <input type="checkbox" onChange={handleToggle} />
+                <input
+                    type="checkbox"
+                    onChange={handleToggle}
+                    checked={isDarkMode}
+                />
                 <span className="slider"></span>
             </label>
         </div>
