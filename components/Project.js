6cfb936f17f6f799c5ae8b1f6084bdddfaef2a7c
papyntown <realistedem@gmail.com>
2023-03-14 10:33:09 +0100

    box-shadow

--- components/Project.js
+++ components/Project.js
@@ -14,33 +14,39 @@ const Project = ({ project }) => {
                 </ul>
             </div>
             <div className="img-content">
-                <div className="img-container hover">
-                    <span>
-                        <div className="head-span">
-                            <h3>{project.title}</h3>
-                            <div className="languages-logo">
-                                {project.languages.map((icon) => (
-                                    <Image
-                                        key={icon}
-                                        src={`../language/${icon}.svg`}
-                                        alt={`Icon language ${icon}`}
-                                        width={100}
-                                        height={100}
-                                        className="img"
-                                    />
-                                ))}
+                <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <div className="img-container hover">
+                        <span>
+                            <div className="head-span">
+                                <h3>{project.title}</h3>
+                                <div className="languages-logo">
+                                    {project.languages.map((icon) => (
+                                        <Image
+                                            key={icon}
+                                            src={`../language/${icon}.svg`}
+                                            alt={`Icon language ${icon}`}
+                                            width={100}
+                                            height={100}
+                                            className="img"
+                                        />
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                        <p>{project.infos}</p>
-                    </span>
-                    <Image
-                        src={`../${project.img}`}
-                        alt={`Img du projet ${project.title}`}
-                        width={100}
-                        height={100}
-                        className="img"
-                    />
-                </div>
+                            <p>{project.infos}</p>
+                        </span>
+
+                        <Image
+                            src={`../${project.img}`}
+                            alt={`Img du projet ${project.title}`}
+                            width={100}
+                            height={100}
+                            className="img"
+                        />
+                    </div>
+                </a>
                 <div className="button-container">
                     <a
                         href={project.link}
